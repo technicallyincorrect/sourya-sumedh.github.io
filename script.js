8b3670b8ece61febfee60299260d12d76b853c5d
technicallyincorrect <123159974+technicallyincorrect@users.noreply.github.com>
2026-07-12 18:17:56 +0200

    Add mobile navigation

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const menuButton = document.getElementById("menuButton");
+const navigationLinks = document.getElementById("navigationLinks");
+const navigationItems = navigationLinks.querySelectorAll("a");
+const currentYear = document.getElementById("currentYear");
 
+menuButton.addEventListener("click", () => {
+    navigationLinks.classList.toggle("open");
+});
+
+navigationItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        navigationLinks.classList.remove("open");
+    });
+});
+
+currentYear.textContent = new Date().getFullYear();
